e5d2671863192566d9d6de98c115584da334b15c
Dmitry Gutkovsky <femiarkh@gmail.com>
2020-12-12 18:00:51 +0300

    init: start covid-dashboard task

---
+++ config/webpack.common.js
@@ -0,0 +1,68 @@
+const paths = require('./paths');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: [paths.src + '/index.js'],
+
+  output: {
+    path: paths.build,
+    filename: '[name].bundle.js',
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: paths.public,
+          to: 'assets',
+          globOptions: {
+            ignore: ['*.DS_Store'],
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+
+    new HtmlWebpackPlugin({
+      title: 'COVID-19 dashboard',
+      favicon: paths.src + '/assets/images/favicon.png',
+      template: paths.src + '/template.html',
+      filename: 'index.html',
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          {loader: 'css-loader', options: {sourceMap: true, importLoaders: 1}},
+          {loader: 'postcss-loader', options: {sourceMap: true}},
+          {loader: 'sass-loader', options: {sourceMap: true}},
+        ],
+      },
+
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+}
