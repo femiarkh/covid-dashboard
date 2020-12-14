97606840cd9fbadfae233854aa2b469a9cf4962a
Dmitry Gutkovsky <femiarkh@gmail.com>
2020-12-14 20:48:34 +0300

    feat: add Switchers class

---
+++ src/js/const/selects.js
@@ -0,0 +1,11 @@
+const SELECTS = {
+  valueName: ['Cases', 'Deaths', 'Recovered'],
+  period: ['All time', 'Last Day'],
+  valueType: ['Absolute', 'Per 100k'],
+  tableSelects: ['period', 'valueType'],
+  mapSelects: ['valueName', 'period', 'valueType'],
+  listSelects: ['period', 'valueType'],
+  chartSelects: ['valueName', 'period', 'valueType'],
+};
+
+export default SELECTS;
