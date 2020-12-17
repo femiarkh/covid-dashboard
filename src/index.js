705b0d437cca7ee42b270b0e7c78f3f02860658e
Dmitry Gutkovsky <femiarkh@gmail.com>
2020-12-17 18:00:10 +0300

    feat: add Covid Chart class and instantiate it in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles/main.scss';
 import CovidTable from './js/covid-table';
+import CovidChart from './js/covid-chart';
 import createElement from './js/utils/create-element';
 
 /**
@@ -10,16 +11,19 @@ class App {
    * App should take Covid Table, Covid Map, Covid List and Covid Chart as its parameters.
    * @param {object} table - instance of Covid Table class.
    */
-  constructor(table) {
+  constructor(table, chart) {
     this.table = table;
+    this.chart = chart;
     this.name = 'Covid-19 Dashboard';
     this.header = createElement('h1', 'app-name', this.name);
     this.container = document.getElementById('root');
     this.table.updateData();
     this.table.bindSelectChange();
+    this.chart.buildChart();
   }
 }
 
-const app = new App(new CovidTable());
+const app = new App(new CovidTable(), new CovidChart());
 app.container.append(app.header);
 app.container.append(app.table.element);
+app.container.append(app.chart.element);
---
+++ src/js/covid-chart.js
@@ -0,0 +1,112 @@
+import createElement from './utils/create-element';
+import Switchers from './switchers';
+import SELECTS from './const/selects';
+import PARAMETERS from './const/parameters';
+import addCommas from './utils/add-commas';
+import countPer100k from './utils/count-per-100k';
+import Chart from '../../node_modules/chart.js/dist/Chart.bundle';
+
+/**
+ * Get markup for the Covid Chart.
+ * @returns {string} String with Covid Chart markup.
+ */
+function getCovidChartMarkup() {
+  const switchers = new Switchers(SELECTS.chartSelects).element;
+  return `${switchers.outerHTML}
+          <canvas class="covid-chart__canvas" width="400"></canvas>`;
+}
+
+/**
+ * Class representing the Covid Chart.
+ */
+export default class CovidChart {
+  constructor() {
+    this.element = createElement('div', 'covid-chart', getCovidChartMarkup());
+    this.ctx = this.element.querySelector('.covid-chart__canvas').getContext('2d');
+
+    this.country = null;
+    this.valueName = PARAMETERS.valueName.cases;
+    this.period = PARAMETERS.period.allTime;
+    this.valueType = PARAMETERS.valueType.absolute;
+  }
+
+  /**
+   * Draw new Covid Chart.
+   * @param {string} country - A country name. If there is none, a global chart will be drawn.
+   * @returns {object} The chart.
+   */
+  buildChart(country) {
+    let url;
+    let populationURL;
+    if (country) {
+      url = `https://disease.sh/v3/covid-19/historical/${country}?lastdays=365`;
+      populationURL = `https://disease.sh/v3/covid-19/countries/${country}?strict=true`;
+    } else {
+      url = 'https://disease.sh/v3/covid-19/historical/all?lastdays=365';
+      populationURL = 'https://disease.sh/v3/covid-19/all';
+    }
+    const urls = [url, populationURL];
+    const requests = urls.map((link) => fetch(link));
+    Promise.all(requests)
+      .then((responses) => Promise.all(responses.map((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.json();
+      })))
+      .then((datasets) => {
+        const mainData = country ? datasets[0].timeline : datasets[0];
+        const { population } = datasets[1];
+        const dataLabels = Object.keys(mainData[this.valueName]).map((key) => new Date(key));
+        let dataValues = Object.values(mainData[this.valueName]);
+        if (this.period === PARAMETERS.period.lastDay) {
+          dataValues = dataValues.map((value, index, arr) => value - arr[index - 1] || 0);
+        }
+        if (this.valueType === PARAMETERS.valueType.per100k) {
+          dataValues = dataValues.map((value) => countPer100k(value, population));
+        }
+        const chart = new Chart(this.ctx, {
+          type: 'line',
+          data: {
+            labels: dataLabels,
+            datasets: [{
+              label: `${this.country || 'All the world'}`,
+              backgroundColor: 'rgb(124, 154, 105)',
+              borderColor: 'rgb(124, 154, 105)',
+              data: dataValues,
+            }],
+          },
+          options: {
+            responsive: true,
+            aspectRatio: 1.5,
+            scales: {
+              xAxes: [{
+                type: 'time',
+                position: 'bottom',
+                offset: true,
+                time: {
+                  unit: 'month',
+                },
+              }],
+              yAxes: [{
+                ticks: {
+                  callback: (value) => addCommas(value),
+                },
+              }],
+            },
+            tooltips: {
+              callbacks: {
+                title: (tooltipItem) => {
+                  const TIME = ', 12:00:00 am';
+                  const timeLength = TIME.length;
+                  return tooltipItem[0].label.slice(0, -timeLength);
+                },
+                label: (tooltipItem) => addCommas(tooltipItem.yLabel),
+              },
+            },
+          },
+        });
+        return chart;
+      });
+  }
+}
