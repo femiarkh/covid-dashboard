7b3522881a4128d4703079096d84eb2b98bb6fc8
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-01-04 11:19:02 +0300

    refactor: put array with countries into const

---
+++ src/js/const/countries.js
@@ -0,0 +1,15 @@
+const COUNTRIES = ['AFG', 'ALB', 'DZA', 'AGO', 'ARG', 'ARM', 'AUS', 'AUT', 'AZE', 'BHS', 'BGD', 'BLR',
+  'BEL', 'BLZ', 'BEN', 'BMU', 'BTN', 'BOL', 'BIH', 'BWA', 'BRA', 'BRN', 'BGR', 'BFA', 'BDI', 'KHM',
+  'CMR', 'CAN', 'CAF', 'TCD', 'CHL', 'CHN', 'COL', 'COG', 'CRI', 'HRV', 'CUB', 'CYP', 'CZE', 'CIV',
+  'COD', 'DNK', 'DJI', 'DOM', 'ECU', 'EGY', 'SLV', 'GNQ', 'ERI', 'EST', 'ETH', 'FLK', 'FJI', 'FIN',
+  'FRA', 'GUF', 'GAB', 'GMB', 'GEO', 'DEU', 'GHA', 'GRC', 'GTM', 'GIN', 'GNB', 'GUY', 'HTI',
+  'HND', 'HUN', 'ISL', 'IND', 'IDN', 'IRN', 'IRQ', 'IRL', 'ISR', 'ITA', 'JAM', 'JPN', 'JOR', 'KAZ',
+  'KEN', 'KWT', 'KGZ', 'LAO', 'LVA', 'LBN', 'LSO', 'LBR', 'LBY', 'LTU', 'LUX', 'MKD', 'MDG', 'MWI',
+  'MYS', 'MLI', 'MLT', 'MRT', 'MEX', 'MDA', 'MNG', 'MNE', 'MAR', 'MOZ', 'MMR', 'NAM', 'NPL', 'NLD',
+  'NCL', 'NZL', 'NIC', 'NGA', 'NOR', 'OMN', 'PAK', 'PSE', 'PAN', 'PNG', 'PRY', 'PER', 'PHL', 'POL',
+  'PRT', 'QAT', 'ROU', 'RUS', 'RWA', 'KOR', 'SAU', 'SEN', 'SRB', 'SLE', 'SVK', 'SVN', 'SLB', 'SOM',
+  'ZAF', 'SSD', 'ESP', 'LKA', 'SDN', 'SUR', 'SWZ', 'SWE', 'CHE', 'SYR', 'TWN', 'TJK', 'TZA', 'THA',
+  'TLS', 'TGO', 'TTO', 'TUN', 'TUR', 'ARE', 'GBR', 'USA', 'UGA', 'UKR', 'URY', 'UZB', 'VUT', 'VEN',
+  'VNM', 'ESH', 'YEM', 'ZMB', 'ZWE', 'NER'];
+
+export default COUNTRIES;
